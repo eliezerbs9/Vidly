0b119c77d340e3961a17b679cb2312499c7264b5
Eliezer Barbosa <eliezerbs9@gmail.com>
2020-03-17 16:56:43 -0700

    Created a form to Add and Edit Movies.

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -35,7 +35,38 @@ namespace Vidly.Controllers
             {
                 return HttpNotFound();
             }
-            return View(movie);
+            var movieViewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+            return View("MovieForm",movieViewModel);
+        }
+
+        public ActionResult New()
+        {
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+             };
+            return View("MovieForm", viewModel);
+        }
+        [HttpPost]
+        public ActionResult Save(Movie movie) {
+            if (movie.Id == 0)
+                _context.Movies.Add(movie);
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.DateAdded = movie.DateAdded;
+                movieInDb.NumbetInStock = movie.NumbetInStock;
+                movieInDb.GenreId = movie.GenreId;
+            }
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Movie");
         }
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,16 +9,25 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        [Display(Name = "Date Released")]
         [Required]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Date Added")]
         [Required]
         public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Number in Stock")]
         [Required]
         public int NumbetInStock { get; set; }
+
         public byte GenreId { get; set; }
+
         public Genre Genre { get; set; }
 
     }
