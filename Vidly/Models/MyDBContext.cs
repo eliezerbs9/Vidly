c07f845e8005654a291a027734ad4bdaa2c9d0fe
Eliezer Barbosa <eliezerbs9@gmail.com>
2020-03-21 00:07:15 -0700

    Fixed problem registering users. Added missing files

--- Vidly/Models/MyDBContext.cs
+++ Vidly/Models/MyDBContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -6,12 +7,23 @@ using System.Web;
 
 namespace Vidly.Models
 {
-    public class MyDBContext : DbContext
+    public class MyDBContext : IdentityDbContext<ApplicationUser>
     {
        public DbSet<Customer> Customers { get; set; }
        public DbSet<Movie> Movies { get; set; }
        public DbSet<MembershipType> MembershipTypes { get; set; }
        public DbSet<Genre> Genres { get; set; }
+
+
+        public MyDBContext()
+            : base("Vidly.Models.MyDBContext", throwIfV1Schema: false)
+        {
+        }
+
+        public static MyDBContext Create()
+        {
+            return new MyDBContext();
+        }
 
     }
 }
---
+++ Vidly/Startup.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly.Startup))]
+namespace Vidly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+            ConfigureAuth(app);
+        }
+    }
+}
